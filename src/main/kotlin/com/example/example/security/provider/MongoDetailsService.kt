b95ac5386d6806129196cdb401caa11e88c93b4a
Vinncent Alexander Wong <vinncentwong2@gmail.com>
2023-03-19 21:36:31 +0700

    [ADD] mongo details service

---
+++ src/main/kotlin/com/example/example/security/provider/MongoDetailsService.kt
@@ -0,0 +1,27 @@
+package com.example.example.security.provider
+
+import com.example.example.app.user.IUserRepository
+import com.example.example.userdetails.CustomUserDetails
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.security.core.userdetails.UserDetailsService
+
+class MongoDetailsService(
+
+    @Autowired
+    val userRepository: IUserRepository
+) : UserDetailsService {
+
+    override fun loadUserByUsername(username: String?): UserDetails {
+        val customUserDetails = CustomUserDetails(null)
+        username?.let {
+            val result = this.userRepository.findById(username)
+            if(result.isPresent){
+                val user = result.get()
+                customUserDetails.user = user
+                return customUserDetails
+            }
+        }
+        return customUserDetails
+    }
+}
