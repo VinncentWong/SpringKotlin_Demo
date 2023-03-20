b3d0e7da1211c2be40d3eeaa802b74582c2e9a84
Vinncent Alexander Wong <vinncentwong2@gmail.com>
2023-03-20 16:38:54 +0700

    [ADD] new implementation from IExtractable

--- src/main/kotlin/com/example/example/entity/User.kt
+++ src/main/kotlin/com/example/example/entity/User.kt
@@ -1,5 +1,6 @@
 package com.example.example.entity
 
+import com.example.example.util.IExtractable
 import org.springframework.data.annotation.CreatedDate
 import org.springframework.data.mongodb.core.mapping.MongoId
 import java.util.*
@@ -13,8 +14,23 @@ data class User(
     var kontakInstagram: String?,
     var username: String?,
     var createdAt: Date,
-    var updatedAt: Date,
+    var updatedAt: Date?,
     var idLike: String?,
     @MongoId
     var id: String?
-)
+): IExtractable{
+    constructor(): this(null, null, null,
+        null, null, null,
+        null, Date(), null, null, null
+    )
+
+    override fun getData(): Map<String, Any?> {
+        val map = mapOf(
+            "id" to this.id,
+            "username" to this.username,
+            "email" to this.email,
+            "createdAt" to this.createdAt
+        )
+        return map
+    }
+}
