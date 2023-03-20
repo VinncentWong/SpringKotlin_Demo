25e7ef7be81d82b5e3580f56ba21ed7dc149c913
Vinncent Alexander Wong <vinncentwong2@gmail.com>
2023-03-20 16:37:51 +0700

    [ADD] jwt util

---
+++ src/main/kotlin/com/example/example/util/JwtUtil.kt
@@ -0,0 +1,24 @@
+package com.example.example.util
+
+import io.jsonwebtoken.Jwts
+import io.jsonwebtoken.SignatureAlgorithm
+import io.jsonwebtoken.security.Keys
+import java.security.Key
+
+object JwtUtil {
+
+    val secretKey: Key = Keys.secretKeyFor(SignatureAlgorithm.HS256)
+
+    fun <T : IExtractable> generateToken(datas: T): String{
+        val data = datas.getData()
+        val claims = Jwts.claims()
+        claims["createdAt"] = data["createdAt"]
+        claims["username"] = data["username"]
+        claims["email"] = data["email"]
+        claims["id"] = data["id"]
+        return Jwts.builder()
+            .addClaims(claims)
+            .signWith(secretKey)
+            .compact()
+    }
+}
