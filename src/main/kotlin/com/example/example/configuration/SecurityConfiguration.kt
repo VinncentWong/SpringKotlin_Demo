ed80302e184c22341f26f7fb8e1086d6332273f6
Vinncent Alexander Wong <vinncentwong2@gmail.com>
2023-03-20 11:17:45 +0700

    [ADD] new request matchers

--- src/main/kotlin/com/example/example/configuration/SecurityConfiguration.kt
+++ src/main/kotlin/com/example/example/configuration/SecurityConfiguration.kt
@@ -1,14 +1,23 @@
 package com.example.example.configuration
 
+import com.example.example.security.filter.JwtFilter
+import com.example.example.security.manager.JwtManager
+import com.example.example.security.provider.JwtProvider
+import com.example.example.security.provider.MongoDetailsService
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configurers.CorsConfigurer
 import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 import org.springframework.web.cors.CorsConfiguration
 
 @Configuration
-class SecurityConfiguration {
+class SecurityConfiguration(
+    @Autowired
+    private val mongoDetailsService: MongoDetailsService
+){
 
     @Bean
     fun securityConfiguration(http: HttpSecurity): SecurityFilterChain{
@@ -26,6 +35,28 @@ class SecurityConfiguration {
                     }
                 }
             }
+            .addFilterBefore(jwtFilter(), UsernamePasswordAuthenticationFilter::class.java)
+            .authorizeHttpRequests {
+                it.requestMatchers("/**/login", "/**/register")
+                    .permitAll()
+                    .anyRequest()
+                    .authenticated()
+            }
             .build()
+    }
+
+    @Bean
+    fun jwtFilter(): JwtFilter{
+        return JwtFilter(jwtManager())
+    }
+
+    @Bean
+    fun jwtManager(): JwtManager{
+        return JwtManager(jwtProvider())
+    }
+
+    @Bean
+    fun jwtProvider(): JwtProvider{
+        return JwtProvider(this.mongoDetailsService)
     }
 }
