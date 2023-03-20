f890cf928f01800612002cc82334cbee9561f4e6
Vinncent Alexander Wong <vinncentwong2@gmail.com>
2023-03-20 12:27:07 +0700

    [ADD] new function for user repository

--- src/main/kotlin/com/example/example/app/user/repository/UserRepository.kt
+++ src/main/kotlin/com/example/example/app/user/repository/UserRepository.kt
@@ -2,7 +2,11 @@ package com.example.example.app.user
 
 import com.example.example.entity.User
 import org.springframework.data.mongodb.repository.MongoRepository
+import org.springframework.data.mongodb.repository.Query
 import org.springframework.stereotype.Repository
 
 @Repository
-interface IUserRepository : MongoRepository<User, String>{}
+interface IUserRepository : MongoRepository<User, String>{
+    @Query("{'\$email' : ?1}")
+    fun findByEmail(email: String): User?
+}
