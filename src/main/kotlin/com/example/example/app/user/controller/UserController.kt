bdee79461f1bba51842619f749e86006abe06ba7
Vinncent Alexander Wong <vinncentwong2@gmail.com>
2023-03-21 15:42:18 +0700

    [ADD] request body annotation

--- src/main/kotlin/com/example/example/app/user/controller/UserController.kt
+++ src/main/kotlin/com/example/example/app/user/controller/UserController.kt
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -20,18 +21,18 @@ class UserController(
 ) : IUserController{
 
     @PostMapping("/create")
-    override fun createUser(@Valid dto: CreateUserDto): ResponseEntity<Response> {
+    override fun createUser(@Valid @RequestBody dto: CreateUserDto): ResponseEntity<Response> {
         return this.service.createUser(dto)
     }
 
     @PostMapping("/login")
-    override fun loginUser(@Valid dto: LoginUserDto): ResponseEntity<Map<String, Any>> {
+    override fun loginUser(@Valid @RequestBody dto: LoginUserDto): ResponseEntity<Map<String, Any>> {
         return this.service.loginUser(dto)
     }
 
     @GetMapping("/get/{id}")
     override fun getUserById(@PathVariable("id") id: String): ResponseEntity<Response> {
-        return this.getUserById(id)
+        return this.service.getUserById(id)
     }
 
     @DeleteMapping("/delete/{id}")
