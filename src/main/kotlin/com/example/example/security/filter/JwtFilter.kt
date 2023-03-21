8d6b7ad3d26bc12665964d3a8e3534fa91e40ad0
Vinncent Alexander Wong <vinncentwong2@gmail.com>
2023-03-21 15:37:12 +0700

    [REFACTOR] to use resolver instead authentication entry point

--- src/main/kotlin/com/example/example/security/filter/JwtFilter.kt
+++ src/main/kotlin/com/example/example/security/filter/JwtFilter.kt
@@ -1,23 +1,30 @@
 package com.example.example.security.filter
 
 import com.example.example.entity.Response
-import com.example.example.security.authentication.JwtAuthentication
+import com.example.example.exception.CustomAuthenticationException
+import com.example.example.security.authentication.NotAuthenticatedJwt
 import com.example.example.security.manager.JwtManager
 import com.fasterxml.jackson.databind.ObjectMapper
+import io.jsonwebtoken.JwtException
 import jakarta.servlet.FilterChain
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.http.HttpStatus
 import org.springframework.security.core.Authentication
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.web.filter.OncePerRequestFilter
+import org.springframework.web.servlet.HandlerExceptionResolver
 
 class JwtFilter(
     @Autowired
     private val manager: JwtManager,
-    private val mapper: ObjectMapper = ObjectMapper()
+    private val resolver: HandlerExceptionResolver
 ) : OncePerRequestFilter(){
+
     override fun doFilterInternal(
         request: HttpServletRequest,
         response: HttpServletResponse,
@@ -27,27 +34,19 @@ class JwtFilter(
         if(bearerToken == null){
             filterChain.doFilter(request, response)
         } else {
-            if (!bearerToken.startsWith("Bearer ")){
-                this.sendErrorResponse(response, "wrong token type", false, HttpStatus.FORBIDDEN)
-                return
+            if (!bearerToken.startsWith("Bearer")){
+                this.resolver.resolveException(request, response, null, JwtException("wrong token type"))
             } else {
                 val token = bearerToken.substring(7)
-                val authentication: Authentication = JwtAuthentication(token, null, null)
+                val authentication: Authentication = NotAuthenticatedJwt(token, null)
                 val result = this.manager.authenticate(authentication)
                 if(!result.isAuthenticated){
-                    this.sendErrorResponse(response, "jwt token is wrong", false, HttpStatus.FORBIDDEN)
+                    this.resolver.resolveException(request, response, null, JwtException(result.principal.toString()))
                 } else {
                     SecurityContextHolder.getContext().authentication = result
                     filterChain.doFilter(request, response)
                 }
             }
         }
-    }
-
-    private fun sendErrorResponse(httpResponse: HttpServletResponse, message: String, success: Boolean, status: HttpStatus){
-        val response = Response(null, success, message)
-        httpResponse.contentType = "application/json"
-        httpResponse.writer.write(this.mapper.writeValueAsString(response))
-        httpResponse.status = status.value()
     }
 }
