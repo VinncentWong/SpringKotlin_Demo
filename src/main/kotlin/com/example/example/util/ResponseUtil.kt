dca6d5447693992640cba8a1cfc55c426667d8e8
Vinncent Alexander Wong <vinncentwong2@gmail.com>
2023-03-21 15:37:53 +0700

    [REFACTOR] login inside sendResponse

--- src/main/kotlin/com/example/example/util/ResponseUtil.kt
+++ src/main/kotlin/com/example/example/util/ResponseUtil.kt
@@ -6,12 +6,17 @@ import org.springframework.http.ResponseEntity
 
 object ResponseUtil{
     fun sendResponse(status: Int, message: String, success: Boolean, datas: Any?): ResponseEntity<Response>{
-        val data = mapOf(
-            "success" to success,
-            "message" to message,
-            "data" to datas
-        )
-        val response = Response(data, success, message)
-        return ResponseEntity.status(status).body(response)
+        if(success){
+            val data = mapOf(
+                "success" to success,
+                "message" to message,
+                "data" to datas
+            )
+            val response = Response(data, success, message)
+            return ResponseEntity.status(status).body(response)
+        } else {
+            val response = Response(null, success, message)
+            return ResponseEntity.status(status).body(response)
+        }
     }
 }
